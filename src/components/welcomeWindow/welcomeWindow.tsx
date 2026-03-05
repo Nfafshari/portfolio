137b2c9fd65fed22c1c9ade85ba0a20ad92aee75
speed-test-table-admin <nathen.afshari@gmail.com>
2026-03-04 19:04:47 -0600

    Update welcomeWindow.tsx

--- src/components/welcomeWindow/welcomeWindow.tsx
+++ src/components/welcomeWindow/welcomeWindow.tsx
@@ -13,26 +13,24 @@ export default function WelcomeWindow () {
                     {
                         pageNum == '1' ?
                             <p className='text-lg font-mono'> 
-                                My Name is Nathen Afshari. I'm from East Grand Forks, MN.
+                                Hi! I'm <span className='font-bold'>Nathen Afshari</span>, a <span className='font-bold'>Senior Computer Science</span> student at the <a href='https://und.edu/' target='_blank'><span className='text-green-700 underline'>University of North Dakota</span></a>, based in East Grand Forks, MN.
                                 <br/>
-                                <br/> I am currently a <span className='font-bold'>Senior</span> at the <a href='https://und.edu/' target='_blank'><span className='text-green-700 underline'>University of North Dakota</span></a> with a Bachelor's Degree in Computer Science.
-                                <br/> I have a passion for software engineering; more specifically, software development, full-stack development, and UI design.
-                                <br/>
-                                <br/> I currently work for <a href='https://www.ideal-aerosmith.com/' target='_blank'><span className='text-orange-500 underline'>Ideal Aerosmith</span></a> as a <span className='font-bold'>Software Engineer Intern</span>  where I have gained meaningful career expierence!
+                                <br/> I'm passionate about <span className='font-bold'>full-stack development</span>, <span className='font-bold'>frontend engineering</span>, and building intuitive user experiences. Currently, I work as a <span className='font-bold'>Software Engineer Intern</span> at <a href='https://www.ideal-aerosmith.com/' target='_blank'><span className='text-orange-500 underline'>Ideal Aerosmith</span></a>, developing desktop applications for multi-axis motion control systems using React and Electron. I also work as a <span className='font-bold'>Software Engineer</span> at the <a href='https://crc.und.edu/' target='_blank'><span className='text-blue-700 underline'>Computational Research Center</span></a>, building authentication, authorization, and API integration features for the UND Arctic Labs research platform using Next.js.
                             </p>
                         :
                             <p className='text-lg font-mono'> 
-                                In my free time I enjoy hanging out with my friends, sports, working out, playing video games, and working on little side projects.
+                                In my free time I enjoy hanging out with friends, sports, working out, gaming, and tackling side projects.
                                 <br/>
                                 <br/>
                                 <br/> 
-                                This portfolio was built using
-                                    <a href='https://react.dev/' target='_blank'> <span className='text-sky-300 underline ml-1'>React</span></a> + 
-                                    <a href='https://vite.dev/' target='_blank'> <span className='text-violet-700 underline ml-1'>Vite</span></a> + 
-                                    <a href='https://www.typescriptlang.org/' target='_blank'> <span className='text-blue-700 underline'>TypeScript</span></a>. Additionally, I used 
-                                    <a href='https://tailwindcss.com/' target='_blank'> <span className='text-sky-700 underline ml-1'>Tailwind.css</span></a> and 
+                                This portfolio was built using:
+                                <br/>
+                                <a href='https://react.dev/' target='_blank'> <span className='text-sky-300 underline'>React</span></a> + 
+                                    <a href='https://vite.dev/' target='_blank'> <span className='text-violet-700 underline'>Vite</span></a> + 
+                                    <a href='https://www.typescriptlang.org/' target='_blank'> <span className='text-blue-700 underline'>TypeScript</span></a>, with styling from
+                                    <a href='https://tailwindcss.com/' target='_blank'> <span className='text-sky-700 underline ml-1'>Tailwind CSS</span></a> and the 
                                     <a href='https://react95.github.io/React95/?path=/story/all--all' target='_blank'> <span className='text-pink-500 underline'>React95</span> </a>
-                                    for this nice <span className='font-sans font-bold text-lg'>Windows</span><span className='font-sans text-red-600 font-bold text-lg'>9</span><span className='font-sans text-blue-800 font-bold text-lg m-0'>5</span> retro look!
+                                    component library for a <span className='font-sans font-bold text-lg'>Windows</span><span className='font-sans text-red-600 font-bold text-lg'>9</span><span className='font-sans text-blue-800 font-bold text-lg m-0'>5</span> feel!
                             </p>
                     }
                     <div className='flex absolute right-0 bottom-0'>
